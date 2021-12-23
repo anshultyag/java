c3e3d5cbef4873544b809241012cda7bcda440a4
anshultyag <you@example.com>
2021-12-23 13:17:55 +0530

    day 3

---
+++ EmpWageBuilderUC1.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class EmpWageBuilderUC1 {
+    public static void main(String[] args) {
+        int isFullTime = 1;
+        double empCheck = Math.floor(Math.random() * 10 ) % 2;
+        if (empCheck == isFullTime)
+            System.out.println("employee is present");
+        else
+            System.out.println("absent");
+
+    }
+}
