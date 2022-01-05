a452a3ad42d3403df6183a5d6113bfa90d62a743
anshultyag <87016992+anshultyag@users.noreply.github.com>
2022-01-05 23:02:44 +0530

    Update CartesianSystemUC01.java

--- CartesianSystemUC01.java
+++ CartesianSystemUC01.java
@@ -31,6 +31,13 @@ public class CartesianSystemUC01 {
 
         double lengthOfLine2= Math.sqrt((lx2 - lx1) ^ 2 + (ly2-ly1) ^ 2);
         System.out.println("Length of a Line2: "+ lengthOfLine2);
+        
+        int val1 = (int) lengthOfLine1;
+        int val2 = (int) lengthOfLine2;
+        Integer x = Integer.valueOf(val1);
+        Integer y = Integer.valueOf(val2);
+
+        System.out.println(x.equals(y));
 
     }
 }
