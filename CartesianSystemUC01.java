0196622c7a31a711f24522a1a697f07eacf78b63
anshultyag <87016992+anshultyag@users.noreply.github.com>
2022-01-05 22:59:35 +0530

    Update CartesianSystemUC01.java

--- CartesianSystemUC01.java
+++ CartesianSystemUC01.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class CartesianSystemUC01 {
+public class CartesianSystemUC03_compare_to {
     public static void main(String[] args) {
 
         //input for first Line
@@ -16,10 +16,10 @@ public class CartesianSystemUC01 {
         System.out.println("Enter y-coordinate of final pont  of line1: ");
         int y2 = sc.nextInt();
 
-        double lengthOfLine1= Math.sqrt((x2 - x1) ^ 2 + (y2-y1) ^ 2);
-         System.out.println("Length of a Line1: "+ lengthOfLine1);
+        double lengthOfLine1 = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+        System.out.println("Length of a Line1: " + lengthOfLine1);
 
-         //input for second line
+        //input for second line
         System.out.println("Enter x-coordinate of intial point  of line2: ");
         int lx1 = sc.nextInt();
         System.out.println("Enter y-coordinate of intial point  of line2: ");
@@ -29,8 +29,19 @@ public class CartesianSystemUC01 {
         System.out.println("Enter y-coordinate of final point  of line2: ");
         int ly2 = sc.nextInt();
 
-        double lengthOfLine2= Math.sqrt((lx2 - lx1) ^ 2 + (ly2-ly1) ^ 2);
-        System.out.println("Length of a Line2: "+ lengthOfLine2);
+        double lengthOfLine2 = Math.sqrt((lx2 - lx1) ^ 2 + (ly2 - ly1) ^ 2);
+        System.out.println("Length of a Line2: " + lengthOfLine2);
 
+        int val1 = (int) lengthOfLine1;
+        int val2 = (int) lengthOfLine2;
+        Integer x = Integer.valueOf(val1);
+        Integer y = Integer.valueOf(val2);
+        int result = x.compareTo(y);
+        if (result > 0 )
+            System.out.println("Line1 is greater than line2");
+        else if(result < 0 )
+            System.out.println("line2 is greater than line1");
+        else
+            System.out.println("line 1 is greater than line 2");
     }
 }
